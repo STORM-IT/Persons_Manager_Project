abb06ff3d063b124c323a87fd579833ad65410ad
storm <stooormix@gmail.com>
2022-01-18 14:09:59 +0330

    destructer {store} from redux/storm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {render} from 'react-dom'
 import { Provider } from 'react-redux';
-import store from './Redux/Store/store'
+import {store} from './Redux/Store/store'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
