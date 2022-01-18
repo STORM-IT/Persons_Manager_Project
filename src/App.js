e4a0ee6ae8c97f84eed7fde96920735453f6449e
storm <stooormix@gmail.com>
2022-01-18 14:17:41 +0330

    add toastContainer in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
+import { ToastContainer } from 'react-toastify'
 import Header from './component/Header/Header'
 
 export default function App(prop)  {
   return (
     <Fragment>
         <Header/>
+        <ToastContainer/>
     </Fragment>
 
   )
