dbaf702e8964f588d5b8eb1e564f956e050e30d5
storm <stooormix@gmail.com>
2022-01-21 18:31:30 +0330

    use jquery in project and set slide up/down from the container PersonMap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import Header from './component/Header/header'
 import PersonsMap from './component/Persons/PersonsMap'
+import $ from 'jquery'
+import { findDOMNode } from 'react-dom'
 
 export default function App(prop)  {
   const showPersons = useSelector(state => state.showPersons)
+  const slide = useRef();
+  const set_Toggle_Slide=(showOrHidden)=>{
+    console.log(showOrHidden)
+    debugger
+    switch (showOrHidden) {
+      case true:
+        $(findDOMNode(slide.current)).slideDown("slow");
+        break;
+      case false:
+        $(findDOMNode(slide.current)).slideUp("slow");
+        break;
+    
+   
+    }
+  }
+  // useEffect(() => {
+
+  // }, []);
   
   return (
     <Fragment>
         <Header/>
-        {showPersons?<PersonsMap/>:null}
         
+  
+        
+        {showPersons?set_Toggle_Slide(true):set_Toggle_Slide(false)}
+
+        
+        
+        <div ref={slide}>
+        <PersonsMap/>
+        </div>
         <ToastContainer/>
     </Fragment>
 
