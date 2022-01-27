3fb0eb2f6803b6739c21d75cb2edaab0d2886669
storm <stooormix@gmail.com>
2022-01-27 17:12:44 +0330

    edit Action/person_age (remove return)

--- src/Redux/Action/person_age.js
+++ src/Redux/Action/person_age.js
@@ -1,11 +1,11 @@
 export const clearAgeInput = () => {
     return async (dispatch) => {
-        return await dispatch({ type: "CLEAR_AGE_INPUT", payload: "" })
+        await dispatch({ type: "CLEAR_AGE_INPUT", payload: "" })
     }
 }
 
 export const setAgePerson = (e) => {
     return async (dispatch) => {
-        return await dispatch({ type: "SET_AGE_PERSON", payload: e.target.value })
+        await dispatch({ type: "SET_AGE_PERSON", payload: e.target.value })
     }
 }
